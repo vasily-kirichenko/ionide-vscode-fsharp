34a3a6345adc22d4b310a982e7f1da13817c2509
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-07-08 09:30:43 -0700

    Enable

--- src/fsharp.fs
+++ src/fsharp.fs
@@ -18,7 +18,7 @@ let activate(disposables: Disposable[]) =
     let p =
         Project.activate ()
         |> Promise.bind(fun _ -> Linter.activate disposables |> unbox)
-       // |> Promise.bind(fun _ -> CodeLens.activate df' disposables |> unbox)
+        |> Promise.bind(fun _ -> CodeLens.activate df' disposables |> unbox)
     window.setStatusBarMessage("F# Ionide initializing", p) |> ignore
     Tooltip.activate df' disposables
     Autocomplete.activate df' disposables
